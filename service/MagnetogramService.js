0730036231edbb99749f9a3cbf3a69efbffb57c0
NikBauer34 <nik.bauer.09@mail.ri>
2024-01-20 09:45:39 +0300

    feature/bug-fixes

--- service/MagnetogramService.js
+++ service/MagnetogramService.js
@@ -42,10 +42,10 @@ class MagnetogramService {
     return pipes
   }
   async createMagnetogram(author, pipe_id, version, title, markup) {
-    const markup = [0, 1, 0]
+    const true_markup = [0, 1, 0]
     let defects_count = 0
     array.forEach(el => el == 0 ? defects_count++ : false)
-    const magnetogram = await MagnetogramModel.create({title, info: [{version, markup, defects_count, date: new Date()}], author, pipe: pipe_id})
+    const magnetogram = await MagnetogramModel.create({title, info: [{version, markup: true_markup, defects_count, date: new Date()}], author, pipe: pipe_id})
     const new_pipe = await PipeService.newMagnetogram(pipe_id, magnetogram._id)
     return magnetogram
   }
